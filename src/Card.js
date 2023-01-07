1835b1b239ab4251c4c9963d5156f42507cc5745
Carlos Mertens <mertenscarlos@yahoo.com>
2023-01-08 00:56:23 +0100

    feat: create Card components with random robots

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+export const Card = ({ name, email, id }) => {
+  return (
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='Robot Avatar' src={`https://robohash.org/${id}?size=200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
